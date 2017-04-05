fc566cec76fc6d5253abefab16c552fcf14aa315
WarrenJames <jameswarren@programmer.net>
2017-04-05 00:10:25 -0700

    More for 22.

---
+++ exl22.py
@@ -0,0 +1,26 @@
+# Exercise 22: What Do You Know So Far?
+
+# **Exercise 0: The Set Up**
+
+#  N/A
+
+# **Exercise 1: A Good First Program**
+print 'print is to print a string onto the terminal'
+
+#**Exercise 2: Comment And Pound Characters**
+
+# <- the octothorpe/pound/hash/mesh symbol comments out a line
+
+# **Exercise 3: Numbers and Math**
+3 + 3 # plus symbol is for addition
+8 - 2 # minus symbol is for subtraction
+48 / 8 # slash symbol is for division
+3 * 3 # asterisk symbol is for multiplication
+99 % 10 # percent symbol is for remainder (also used as modulo/modulus)
+9 < 11 # less-than symbol
+10 > 2 # greater-than symbol
+6 <= 24 # less-than-equal symbol
+10 >= 12 # greater-than-equal symbol
+
+# **Excercise 4: Variables and Names**
+underscores_in_variables = 1337
