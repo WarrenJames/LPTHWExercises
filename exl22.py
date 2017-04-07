e6b6af36fc32b2c63772f01f85ac1c23f8ea4b5f
WarrenJames <jameswarren@programmer.net>
2017-04-06 20:50:30 -0700

    Exl22

--- exl22.py
+++ exl22.py
@@ -29,5 +29,37 @@ floatingPoint = 7.0 # numbers with decimals
 integer = 7 # numbers without decimals / whole numbers
 
 # **Exercise 5: More Variables and Printing**
+rOne = 'representation modulus'
+rTwo = 'raw data'
 
+print "percent sign that gives value to the string is called a %s" % ('modulus')
 print "a %s modulus is to put value to a %s" % ('string', 'string')
+print "a modulus for numbers(digits) is %d " % underscores_in_variables
+print "a %r is for %r, useful for debugging. Prints with quotes" % (rOne, rTwo)
+
+# **Exercise 6: Strings and Text
+
+print "double-quotes"
+print 'single-quotes'
+
+# **Exercise 7: More Printing**
+
+plusOne = 'the plus symbol can be used for joining strings with'
+plusTwo = 'out spaces.'
+commaOne = 'The comma can be used for joining strings on same line with spaces'
+
+print plusOne + plusTwo,
+print commaOne
+
+# **Exercise 8: Printing, Printing**
+print "%r is a Boolean value that doesn't need quotes" % True
+print "%r is also a Boolean value that doesn't need quotes" % False
+
+# **Exercise 9: Printing, Printing, Printing**
+print "the backslash is used to escape characters"
+print "use the command \n to print on a new line"
+print
+    """ triple-quotes gives you the ability to set off
+    multiple lines of text"""
+
+# **Exercise 10: What was that?**
