ca0a067631e1f2087d41bb128f203fd00b592cd1
WarrenJames <jameswarren@programmer.net>
2017-05-20 19:52:44 -0700

    520

--- Europa2.py
+++ Europa2.py
@@ -1,30 +1,51 @@
-# Europa
-# Designed to improve overall happiness
+# Callisto
+# Mood Stablizer
 
 from sys import argv
 
-script, filename, filename2 = argv
+script = argv
 
-good = open(filename, 'r')
-bad = open(filename2, 'r')
-prompt = '> '
+sad = ['sad', 'tired', 'bored', 'lonely', 'depressed', 'ashamed',
+'guilty', 'sleepy', 'apathetic', 'isolated', 'inferior', 'stupid',
+'remorseful']
+mad = ['mad', 'hurt', 'hostile', 'angry', 'selfish', 'hateful', 'critical',
+'distant', 'sarcastic', 'frustrated', 'jealous', 'irritated', 'skeptical']
+scared = ['scared', 'confused', 'rejected', 'helpless', 'submissive', 'insecure',
+'anxious', 'bewildered', 'discouraged', 'insignificant', 'inadequate',
+'embarrassed', 'overwhelmed']
+joyful = ['joyful', 'excited', 'sensuous', 'energetic', 'cheerful', 'creative',
+'hopeful', 'daring', 'fascinating', 'stimulating', 'amused', 'playful',
+'optimistic', 'happy']
+powerful = ['powerful', 'aware', 'proud', 'respected', 'appreciated', 'important',
+'faithful', 'confident', 'discerning', 'valueable', 'worthwhile', 'successful',
+'surprised']
+peaceful = ['peaceful', 'content', 'thoughful', 'intimate', 'loving', 'trusting',
+'nurturing', 'relaxed', 'pensive', 'responsive', 'serene', 'secure', 'thankful']
 
-print "Hi there my name is %s, What is yours?" % (script)
-user_name = raw_input(prompt)
+def start():
+    print "Hi there my name is %s, What is yours?" % (script)
+    user_name = raw_input("> ")
+    print "Pleased to meet you %s" % (user_name)
+    fq()
 
-qmood = "Good or Bad?"
+def fq():
+    print "How are you feeling today?"
+    mood = raw_input("> ")
 
-print """Pleased to meet you %s, how are you feeling today?
-%s""" % (user_name, qmood)
+    if mood.lower() in sad:
+        print "Sad!"
+    elif mood.lower() in mad:
+        print "Mad!"
+    elif mood.lower() in scared:
+        print "Scared!"
+    elif mood.lower() in joyful:
+        print "Joyful!"
+    elif mood.lower() in powerful:
+        print "Powerful!"
+    elif mood.lower() in peaceful:
+        print "Peaceful!"
+    else:
+        print "Invalid input. Try typing your current emotion."
+        fq()
 
-mood = raw_input(prompt)
-
-good_one = good.readline()
-bad_one = bad.readline()
-
-if mood == ("good"):
-    print good_one
-elif mood == ("bad"):
-    print bad_one
-else:
-    print "wrong input"
+start()
---
+++ exl36.py
@@ -0,0 +1,55 @@
+# Exercise 36: Designing and Debugging
+
+
+"""
+Rules for If-Statements
+1.  Every if-statement must have an else.
+2.  If this else should never run because it doesn't make sense, then you must
+use a die function in the else that prints out an error message and dies.
+3.  Never nest if-statements more than two deep and always try to do them one deep.
+4.  Treat if-statements like paragraphs, where each if-elif-else grouping is like
+a set of sentences. Put blank links before and after.
+5. Your boolean test should be simple. If they are complex, move their calculations
+to variables earlier in your function and use a good name for variable.
+
+"""
+
+"""
+1.  Use a while-loop only to loop forever, and that means probably never. This
+only applies to Python; other languages are different.
+2.  Use a for-loop for all other kinds of looping, especially if there is a fixed
+or limited number of things to loop over.
+
+"""
+
+"""
+Tips for Debugging
+1.  Do not use a "debugger." A debugger is like doing a full-body scan on a sick
+person. You do not get any specific useful information, and you find a while lot
+of information that doesn't help and is just confusing.
+2.  The best way to debug a program is to use print to print out the values of
+variables at points in the program to see where they go wrong.
+3.  Make sure parts of your programs work as you work on them. Do not write
+massive files of code before you try to run them. Code a little, run a little,
+fix a little.
+
+"""
+
+"""
+The best way to work on a piece of software in small chunks:
+1.  On a sheet of paper or an index card, write a list of tasks you need to
+complete to finish the software. This is your to do list.
+2.  Pick the easiest thing you can do from your list.
+3.  Write out English comments in your source file as a guide for how you would
+accomplish this task in your code.
+4.  Write some of the code under the English comments.
+5.  Quickly run your script to see if that code worked.
+6.  Keep working in a cycle of writing some code, running it to test it, and
+fixing it until it works.
+7. Cross this task off your list, then pick your next easiest task and repeat.
+
+This process will help you work on software in a methodical and consistent
+manner. As you work, update your list by removing tasks you don't really need
+and adding ones you do.
+
+"""
