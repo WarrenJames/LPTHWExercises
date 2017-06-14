b9121dd1cdc1db855a269b0581a00a3dbb106dab
WarrenJames <jameswarren@programmer.net>
2017-06-13 23:32:17 -0700

    613.

---
+++ exl39.py
@@ -0,0 +1,22 @@
+# stuff variable is a dictionary 'name' key is "James", 'age' key is 24,
+# 'height' key is "6'2"
+stuff = {'name': "James", 'age': 24, 'height': "6'2"}
+
+# prints name key from dictionary in stuff variable
+# prints as: James
+print stuff['name']
+
+# prints age key from dictionary in stuff variable
+# prints as: 25
+print stuff['age']
+
+# prints height key from dictionary in stuff variable
+# prints as: 6'2
+print stuff['height']
+
+# adds city key into dictionary in stuff variable and assigns it value of "Las Vegas"
+stuff['city'] = "Las Vegas"
+
+# prints city key from dictionary in stuff variable
+# prints as: Las Vegas
+print stuff['city']
